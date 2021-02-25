f97f8a5d5c531aba51038b81dc0123db21ef6a63
HENRY VO <HENRYVO94@GMAIL.com>
2021-02-25 22:12:35 +1100

    Add button icon and theme states

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, Prop, h, Element } from '@stencil/core';
 
 @Component({
   tag: 'g-button',
@@ -7,9 +7,27 @@ import { Component, Prop, h } from '@stencil/core';
 })
 
 export class Button {
+  @Element() element: HTMLElement;
+  
+  @Prop() theme: 'primary' | 'secondary' | 'outlined' | 'naked';
+
+  hasIconOnly() {
+    return Boolean(this.element.querySelector('[slot="icon-only"]'));
+  }
+
   render() {
     return (
-      <button class="button">
+      <button 
+        class={{
+          'button': true,
+          'button--primary': this.theme === 'primary',
+          'button--secondary': this.theme === 'secondary',
+          'button--outlined': this.theme === 'outlined',
+          'button--naked': this.theme === 'naked',
+          'button-has-icon-only': this.hasIconOnly(),
+        }}
+      >
+        <slot name="icon-only" />
         <slot />
       </button>
     );
